b0ecfe60bbdfeda0d163d22fc29491197d48b8d8
EnzoLFranco <fenzolemos@gmail.com>
2025-07-23 16:36:46 -0300

    feat: Cria os métodos generateMoney e getFunds

--- projectPOO/projectPOO/src/main/java/com/enzolfranco/projectPOO/model/Wallet.java
+++ projectPOO/projectPOO/src/main/java/com/enzolfranco/projectPOO/model/Wallet.java
@@ -3,8 +3,11 @@ package com.enzolfranco.projectPOO.model;
 import lombok.Getter;
 import lombok.ToString;
 
+import java.time.OffsetDateTime;
 import java.util.ArrayList;
 import java.util.List;
+import java.util.UUID;
+import java.util.stream.Stream;
 
 @ToString
 public abstract class Wallet {
@@ -17,5 +20,14 @@ public abstract class Wallet {
     public Wallet(BankService serviceType) {
         this.serviceType = serviceType;
         this.money = new ArrayList<>();
+    }
+
+    protected List<Money> generateMoney(final long amount, final String description){
+        var history = new MoneyAudit(UUID.randomUUID(), serviceType, description, OffsetDateTime.now());
+        return Stream.generate(() -> new Money(history)).limit(amount).toList();
+    }
+
+    public long getFunds(){
+        return money.size();
     }
 }
