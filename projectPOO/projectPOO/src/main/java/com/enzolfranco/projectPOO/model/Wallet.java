4a56f87f485cc446b074c07420266415dd04653e
EnzoLFranco <fenzolemos@gmail.com>
2025-07-23 16:46:19 -0300

    feat: Adiciona os métodos de manipulação de Money referentes a Wallet

--- projectPOO/projectPOO/src/main/java/com/enzolfranco/projectPOO/model/Wallet.java
+++ projectPOO/projectPOO/src/main/java/com/enzolfranco/projectPOO/model/Wallet.java
@@ -13,21 +13,39 @@ import java.util.stream.Stream;
 public abstract class Wallet {
 
     @Getter
-    private final BankService serviceType;
+    private final BankService service;
 
     protected final List<Money> money;
 
     public Wallet(BankService serviceType) {
-        this.serviceType = serviceType;
+        this.service = serviceType;
         this.money = new ArrayList<>();
     }
 
     protected List<Money> generateMoney(final long amount, final String description){
-        var history = new MoneyAudit(UUID.randomUUID(), serviceType, description, OffsetDateTime.now());
+        var history = new MoneyAudit(UUID.randomUUID(), service, description, OffsetDateTime.now());
         return Stream.generate(() -> new Money(history)).limit(amount).toList();
     }
 
     public long getFunds(){
         return money.size();
+    }
+
+    public void addMoney(final List<Money> money, final BankService service, final String description){
+        var history = new MoneyAudit(UUID.randomUUID(), service, description, OffsetDateTime.now());
+        money.forEach(m -> m.addHistory(history));
+        this.money.addAll(money);
+    }
+
+    public List<Money> reduceMoney(final long amount){
+        List<Money> toRemove = new ArrayList<>();
+        for (int i = 0; i < amount; i++){
+            toRemove.add(this.money.removeFirst());
+        }
+        return toRemove;
+    }
+
+    public List<MoneyAudit> getFinancialTransactions(){
+        return money.stream().flatMap(m -> m.getHistory().stream()).toList();
     }
 }
