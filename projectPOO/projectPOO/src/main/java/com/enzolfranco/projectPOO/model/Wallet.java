40c27121c109ff466b8c212339f8e2aae6846fb6
EnzoLFranco <fenzolemos@gmail.com>
2025-07-23 16:27:40 -0300

    feat: Define uma list de Money e adiciona o construtor

--- projectPOO/projectPOO/src/main/java/com/enzolfranco/projectPOO/model/Wallet.java
+++ projectPOO/projectPOO/src/main/java/com/enzolfranco/projectPOO/model/Wallet.java
@@ -3,12 +3,19 @@ package com.enzolfranco.projectPOO.model;
 import lombok.Getter;
 import lombok.ToString;
 
+import java.util.ArrayList;
+import java.util.List;
+
 @ToString
 public abstract class Wallet {
 
     @Getter
     private final BankService serviceType;
 
-    
+    protected final List<Money> money;
 
+    public Wallet(BankService serviceType) {
+        this.serviceType = serviceType;
+        this.money = new ArrayList<>();
+    }
 }
