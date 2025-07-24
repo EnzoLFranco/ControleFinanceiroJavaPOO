3e79fe2cff3450c550905c1130c38b424185504c
EnzoLFranco <fenzolemos@gmail.com>
2025-07-24 14:18:14 -0300

    fix: Adiciona a annotation de Getter na AccountWallet

--- projectPOO/projectPOO/src/main/java/com/enzolfranco/projectPOO/model/AccountWallet.java
+++ projectPOO/projectPOO/src/main/java/com/enzolfranco/projectPOO/model/AccountWallet.java
@@ -1,9 +1,12 @@
 package com.enzolfranco.projectPOO.model;
 
+import lombok.Getter;
+
 import java.util.List;
 
 import static com.enzolfranco.projectPOO.model.BankService.ACCOUNT;
 
+@Getter
 public class AccountWallet extends Wallet{
 
     private final List<String> pix;
